2f5efdb1a43c6813c78a47a8c57be353f2f1412b
peter-tharwat <ptharwat@gmail.com>
2020-08-23 09:02:47 +0200

    users

--- app/User.php
+++ app/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     protected $guarded=['id'];
     
     protected $fillable = [
-        'UserName', 'email', 'password','is_Agent','img'
+        'UserName', 'email', 'password','is_Agent','img','postion','phone'
     ];
 
     protected $hidden = [
